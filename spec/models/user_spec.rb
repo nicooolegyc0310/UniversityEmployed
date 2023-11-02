5d99d1f503cd78b56ca82662dfe22f91c7fbc597
rew2155 <135544795+rew2155@users.noreply.github.com>
2023-11-01 23:53:42 -0400

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a student' do
+    student = User.create(username: 'student_username', password: 'password', user_type: 'student')
+    expect(student).to be_valid
+    expect(student.user_type).to eq('student')
+  end
+
+  it 'creates a professor' do
+    professor = User.create(username: 'professor_username', password: 'password', user_type: 'professor')
+    expect(professor).to be_valid
+    expect(professor.user_type).to eq('professor')
+  end
 end
