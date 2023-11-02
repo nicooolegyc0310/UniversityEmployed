24f9c9c30beaf86b71985d29ebad8f7aa0adfec5
Jacob Blindenbach <jblindenbach@nygenome.org>
2023-11-02 03:05:13 +0000

    Fix issue causing heroku to crash

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,4 @@
-class Application < ApplicationRecord
+class Application < ActiveRecord::Base
     belongs_to :user
     belongs_to :opportunity
     validates :status, presence: true, inclusion: { in: %w(pending approved rejected), message: "%{value} is not a valid status" }
