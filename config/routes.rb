64eab4bfd8ac12cc82d222ecacd97ffc1ed8b99d
Yuchen Ge <yg2838@columbia.edu>
2023-11-15 15:03:13 -0500

    Application

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,21 @@ Rottenpotatoes::Application.routes.draw do
   
   #root :to => redirect('/opportunities')
 
+  # root routes
   root 'sessions#login'
 
+  # user routes
   resources :users, only: [:new, :create, :edit, :update, :index, :destroy]
 
+  # session routes
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
 
+  # sign up route
   get '/signup', to: 'users#new', as: 'signup'
+
+  # applications routes
+  resources :applications, only: [:index]
 end
