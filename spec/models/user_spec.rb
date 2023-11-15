28f036175bd7f5855e511576aa8d436994d20cb0
Jacob Blindenbach <jblindenbach@nygenome.org>
2023-11-15 03:20:47 +0000

    fix rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'creates a student' do
-    student = User.create(username: 'student_username', password: 'password', user_type: 'student')
+    student = User.create(email: 'test_user', firstname: "John", lastname: "Smith", password: 'password', user_type: 'student')
     expect(student).to be_valid
     expect(student.user_type).to eq('student')
   end
 
   it 'creates a professor' do
-    professor = User.create(username: 'professor_username', password: 'password', user_type: 'professor')
+    professor = User.create(email: 'test_user', firstname: "John", lastname: "Smith", password: 'password', user_type: 'professor')
     expect(professor).to be_valid
     expect(professor.user_type).to eq('professor')
   end
